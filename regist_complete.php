7e063a5ecfabcf76109e7a1dcb075adfba835363
kuuma-0821 <kuma.s.r.0821@gmail.com>
2024-03-04 22:47:12 +0900

    -完了版

--- regist_complete.php
+++ regist_complete.php
@@ -6,13 +6,14 @@
     }
 
     $gender = $_POST['seibetu']=="女" ? 1 : 0;
+    $account = $_POST['aka']=="管理者" ? 1 : 0;
 
 mb_internal_encoding("utf8");
 
 $pdo= new PDO("mysql:dbname=lesson01;host=localhost;","root","");
 
 $pdo->exec("insert into account_registration(family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority)values
-('".$_POST['namesei']."','".$_POST['namemei']."','".$_POST['kanasei']."','".$_POST['kanamei']."','".$_POST['email1']."','".$pass_after."','".$gender."','".$_POST['yuubinn']."','".$_POST['kenn']."','".$_POST['juusyosiku']."','".$_POST['juusyobann']."','".$_POST['aka']."');");
+('".$_POST['namesei']."','".$_POST['namemei']."','".$_POST['kanasei']."','".$_POST['kanamei']."','".$_POST['email1']."','".$pass_after."','".$gender."','".$_POST['yuubinn']."','".$_POST['kenn']."','".$_POST['juusyosiku']."','".$_POST['juusyobann']."','".$account."');");
 
 ?>
 
@@ -24,7 +25,7 @@ $pdo->exec("insert into account_registration(family_name,last_name,family_name_k
 <head>
     <meta charset="UTF-8">
     <title>アカウント登録画面</title>
-    <link rel="stylesheet" type="text/css" href="style6.css">
+    <link rel="stylesheet" type="text/css" href="style8%20.css">
 </head>
 
 <body>
@@ -32,6 +33,7 @@ $pdo->exec("insert into account_registration(family_name,last_name,family_name_k
     <div class="logo_img">
     <img src="diblog_logo.jpg">
     </div>
+    
     
 <header>
     <div class="logo">
@@ -47,9 +49,12 @@ $pdo->exec("insert into account_registration(family_name,last_name,family_name_k
 </header>
    
     <h1>アカウント登録完了画面</h1>
-    <?php echo $gender ?>
-    <h2>登録完了しました</h2>
+    <div class="kannryou">登録完了しました</div>
     
+    
+    <div class="botan">
+        <input type="submit" onclick="location.href='http://localhost/account/DIブログ .html'" value="TOPページへ戻る" id="top" name="submit1">
+    </div>
     
     
     
